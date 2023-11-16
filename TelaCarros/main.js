f06b940e7b12412f7b6447c94edc8e56aff8a9a5
Lucas <127799897+lucasgribeiro5@users.noreply.github.com>
2023-11-16 13:05:54 -0300

    Add files via upload

---
+++ TelaCarros/main.js
@@ -0,0 +1,13 @@
+function highlightCard(card) {
+    card.style.boxShadow = "0 12px 20px rgba(0, 0, 0, 0.3)";
+  }
+  
+  function resetCard(card) {
+    card.style.boxShadow = "0 8px 16px rgba(0, 0, 0, 0.2)";
+  }
+  
+  function redirectToRentPage() {
+    // Altere o URL para a página de aluguel
+    window.location.href = "pagina-de-aluguel.html";
+  }
+  
